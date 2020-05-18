5d461cefeb7c79dedacd8105734af9ec45775985
kisdyoo <65108334+kisdyoo@users.noreply.github.com>
2020-05-18 19:43:27 +0700

    Add files via upload

---
+++ RemoveTextDeQuy/RemoveTextDeQuy/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace RemoveTextDeQuy
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.textBox3);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(741, 476);
+			this.panel1.TabIndex = 0;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(35, 288);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 22);
+			this.textBox3.TabIndex = 3;
+			this.textBox3.Text = "Đây là TextBox";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(35, 224);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 2;
+			this.textBox2.Text = "Đây là TextBox";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(35, 169);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "Đây là TextBox";
+			// 
+			// panel2
+			// 
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel2.Controls.Add(this.textBox4);
+			this.panel2.Controls.Add(this.textBox5);
+			this.panel2.Controls.Add(this.textBox6);
+			this.panel2.Controls.Add(this.panel3);
+			this.panel2.Location = new System.Drawing.Point(157, 28);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(568, 429);
+			this.panel2.TabIndex = 0;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(42, 260);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 22);
+			this.textBox4.TabIndex = 6;
+			this.textBox4.Text = "Đây là TextBox";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(42, 196);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 22);
+			this.textBox5.TabIndex = 5;
+			this.textBox5.Text = "Đây là TextBox";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(42, 141);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 22);
+			this.textBox6.TabIndex = 4;
+			this.textBox6.Text = "Đây là TextBox";
+			// 
+			// panel3
+			// 
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel3.Controls.Add(this.textBox7);
+			this.panel3.Controls.Add(this.textBox8);
+			this.panel3.Controls.Add(this.textBox9);
+			this.panel3.Location = new System.Drawing.Point(179, 33);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(368, 367);
+			this.panel3.TabIndex = 0;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(37, 227);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 22);
+			this.textBox7.TabIndex = 6;
+			this.textBox7.Text = "Đây là TextBox";
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(37, 163);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(100, 22);
+			this.textBox8.TabIndex = 5;
+			this.textBox8.Text = "Đây là TextBox";
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(37, 108);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(100, 22);
+			this.textBox9.TabIndex = 4;
+			this.textBox9.Text = "Đây là TextBox";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(794, 181);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(124, 77);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "XÓA TOÀN BỘ TEXTBOX";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(930, 508);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ RemoveTextDeQuy/RemoveTextDeQuy/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RemoveTextDeQuy
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			XoaControlTextBox(this);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+		public void XoaControlTextBox(Control ctrl)
+		{
+			foreach (Control item in ctrl.Controls.OfType<Panel>())
+			{
+				if (item.Controls.Count == 0)
+					break;
+				foreach (Control txtbox in item.Controls.OfType<TextBox>())
+				{
+					txtbox.Text = string.Empty;
+				}
+				XoaControlTextBox(item);
+			}
+		}
+	}
+}
+
